b379974c25b0c8f8747c44352ad7d7b6a2cf07f0
daniel-hrckson <daniel.henrickson@protonmail.com>
2020-10-08 20:34:44 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # api-csv-to-json
-It is an api that converts a file csv to an json.
+It is an api that converts a file csv to an json built with Flask.
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, make_response, session, render_template, abort, url_for
+from flask.json import jsonify, loads
+from sys import getsizeof
+import pandas as pd
+
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['csv_file']
+
+        if (file.content_type == 'text/csv') and (getsizeof(file) < 32000000):
+            json_file = pd.read_csv(file).to_json()
+            return loads(json_file)
+        else:
+            return abort(400)
+
+    elif request.method == 'GET':
+        return render_template('index.html')
+
+    else:
+        return abort(400) 
